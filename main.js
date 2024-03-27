96f50764e4ffc006a4c7add4c90241b1a5288d4d
Shruti Pawar <92929103+SP1812-git@users.noreply.github.com>
2024-03-27 14:42:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new typed(".text",{
+    Strings:["Frontend Developer","Backend Developer","Data Analyst"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
